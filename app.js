d881f51aeb01dd373c815017fad4208a09f6cabf
Felipe Barbosa Carvalho <barbosacarvalhodoamaralfelipe@gmail.com>
2024-11-15 13:17:04 -0300

    Condicionais e concatenação

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
-prom
+let numeroSecreto = 4;
+console.log(numeroSecreto);
+let chute = prompt('Escolha um numero entre 1 e 10');
+
+
+if (numeroSecreto == chute){
+    console.log(`Isso ai! você acertou o numero secreto (${numeroSecreto})`);
+} else{
+    alert ('Você errou :(');
+}
