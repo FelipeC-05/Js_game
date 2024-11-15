a87dcd3f7ccdae3e9c7434d7c6b502eb7c6bcbab
Felipe Barbosa Carvalho <barbosacarvalhodoamaralfelipe@gmail.com>
2024-11-15 14:55:21 -0300

    Loops e tentativas

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 4;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um numero entre 1 e 10');
+let chute;
+let tentativas = 1;
 
 
-if (numeroSecreto == chute){
-    console.log(`Isso ai! você acertou o numero secreto (${numeroSecreto})`);
-} else{
-    alert ('Você errou :(');
+while (chute != numeroSecreto){
+    chute =prompt('Escolha um numero entre 1 e 10');
+    if (numeroSecreto == chute){
+        alert(`Isso ai! você acertou o numero secreto (${numeroSecreto}) com ${tentativas} tentativas`);
+    } else{
+        if(chute > numeroSecreto){
+            alert(`o numero secreto é menor que ${chute}`);
+        }
+        else{
+            alert(`o numero secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
 }
